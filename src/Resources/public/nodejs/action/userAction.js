52a775120d180c497fa06dceebd651179728c70e
Acrack <yehya.latti@gmail.com>
2016-10-13 15:27:20 +0200

    nodejs

---
+++ src/Resources/public/nodejs/action/userAction.js
@@ -0,0 +1,25 @@
+var
+    core         = require('../core'),
+    redis        = require('redis'),
+    redisManager = require('../manager/redisManager');
+
+module.exports = {
+    loaded: function (socket, user) {
+        'use strict';
+
+        var appUser = {
+            id         : user.id,
+            session_id : user.sessionId,
+            socket_id  : socket.id,
+            last_login :  (new Date()).getTime()
+        };
+
+        redisManager.deleteUserKeys("user:" + user.id + "_*");
+
+        redisManager.addUser("user:" + appUser.id + "_" + appUser.socket_id, appUser);
+    },
+    disconnect: function (socket) {
+        redisManager.deleteUserKeys("user:[0-9]*" + "_" + socket.id);
+    }
+};
+
